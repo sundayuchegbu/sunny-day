bacf25f7ced1c72f6e356fc2e330878b8d07f581
sundayuchegbu <sundayuchegbu10@gmail.com>
2023-05-27 20:30:05 +0100

    anly commit

--- backend/app.js
+++ backend/app.js
@@ -34,13 +34,11 @@ app.use("/api/v1", payment);
 
 app.use("/api/v1", order);
 
-if (process.env.NODE_ENV === "PRODUCTION") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-  });
-}
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.use(errorMiddleware);
 
